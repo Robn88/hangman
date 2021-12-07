126ee31c4fa81127f910b94fa818c89d270af30b
Robn88 <robert.d.nicholas@gmail.com>
2021-12-07 19:48:55 +0000

    Add welcome to game function and input name

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("Welcome to hangman!")
+
+
+def welcome_to_game():
+    while name.isalpha() is not True:
+        input(f"Sorry, {name} is not a valid name.Please try again:")
+        if name.isalpha() is True:
+            print(f"Hello, {name}!")
+            break
+
+
+name = input("Please enter your name:")
+welcome_to_game()
+
+
+"""
+def welcome_to_game():
+    name = input("Please enter your name:")
+    while name.isalpha() is not True:
+        input(f"Sorry, {name} is not a valid name.Please try again:")
+    if name.isalpha():
+        print(f"Hello, {name}!")  
+"""
