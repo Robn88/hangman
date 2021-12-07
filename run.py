cc8dfc1fb97cd7b27f5f5cf9e50b6dc14d3a3981
Robn88 <robert.d.nicholas@gmail.com>
2021-12-07 21:35:16 +0000

    Add while loop to welcome to game function

--- run.py
+++ run.py
@@ -1,26 +1,16 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("Welcome to hangman!")
-
 
 def welcome_to_game():
-    while name.isalpha() is not True:
-        input(f"Sorry, {name} is not a valid name.Please try again:")
+    while True:
+        name = input("Please enter your name:")
         if name.isalpha() is True:
             print(f"Hello, {name}!")
             break
+        else:
+            print(f"Sorry, {name} is not a valid name.Please try again.")
 
 
-name = input("Please enter your name:")
+print("Welcome to hangman!")
 welcome_to_game()
-
-
-"""
-def welcome_to_game():
-    name = input("Please enter your name:")
-    while name.isalpha() is not True:
-        input(f"Sorry, {name} is not a valid name.Please try again:")
-    if name.isalpha():
-        print(f"Hello, {name}!")  
-"""
