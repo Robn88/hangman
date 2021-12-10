7d689d53db110eca4a67f98acaddd22c4f71373b
Robn88 <robert.d.nicholas@gmail.com>
2021-12-10 14:17:27 +0000

    Fix issue with not adding letter on incorrect guess

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import random
 
-
+"""
 def welcome_to_game():
     while True:
         name = input("Please enter your name:")
@@ -9,17 +9,16 @@ def welcome_to_game():
             break
         else:
             print(f"Sorry, {name} is not a valid name.Please try again.")
-
+"""
 
 print("Welcome to hangman!")
-welcome_to_game()
+# welcome_to_game()
 
 easy_words = []
 get_words = open('easy.txt').read().split()
 for word in get_words:
     easy_words.append(word)
 
-print(easy_words)
 
 word_to_guess = random.choice(easy_words)
 
@@ -35,21 +34,28 @@ print(create_hidden_word())
 
 def main():
     hidden_word = "_" * len(word_to_guess)
+    print(hidden_word)
+    print(word_to_guess)
     already_guessed_letters = []
-    lives_remaining = 10
+    # lives_remaining = 10
     word_not_guessed = True
     while word_not_guessed:
         guess = input("Please enter a letter:\n")
         if len(guess) == 1 and guess.isalpha():
             if guess in already_guessed_letters:
                 print(f"You have already guessed {guess} ")
-        elif guess in hidden_word:
-            print(f"Well done, {guess} is in the word!")
-            already_guessed_letters.append(guess)
-            for i, letter in enumerate(word_to_guess):
-                if letter != "_" and guess == letter:
-                    word_to_guess[i] = letter
-                    print("".join(word_to_guess))
+            elif guess not in word_to_guess:
+                print(f"Bad luck! {guess} is not in the word!")
+                already_guessed_letters.append(guess)
+                print(f"Already guessed: {already_guessed_letters} ")
+            else:
+                print(f"Well done, {guess} is in the word!")
+                already_guessed_letters.append(guess)
+                print(already_guessed_letters)
+                for i, letter in enumerate(word_to_guess):
+                    if letter != "_" and guess == letter:
+                        word_to_guess[i] = letter
+                        print("".join(word_to_guess))
         
 
 main()
